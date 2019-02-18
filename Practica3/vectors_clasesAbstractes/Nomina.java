6fac71499b38b1a8cf8a21218c8aa157df96ee67
APachon10 <Pachondestroy95@gmail.com>
2019-02-18 12:10:16 +0100

    Practica 3 Ejercicio 2 a Medias

--- Practica3/vectors_clasesAbstractes/Nomina.java
+++ Practica3/vectors_clasesAbstractes/Nomina.java
@@ -1,8 +1,33 @@
 package vectors_clasesAbstractes;
 
+import java.util.Scanner;
+
 public class Nomina{
-	public static void main(String[] args) {
-		
+	Tipus_Empleat empleados [] = new Tipus_Empleat[5];
+	//Constructors
+	
+	//Metodos
+	public Tipus_Empleat insereixEmpleat(Tipus_Empleat e ) {
+		boolean salir=false;
+		int op=0;
+		do {
+			
+		} while (!salir);
+		return e;
+	}
+	public void eliminaNeteja() {
+		for (int i = 0; i < empleados.length; i++) {
+			if (empleados[i] instanceof Neteja) {
+				System.out.println("Hola");
+			}
+		}
+	}
+	public void quantsCornella() {
+		for (int i = 0; i < empleados.length; i++) {
+			if (empleados[i].getCiutatO().equalsIgnoreCase("Cornella")) {
+				System.out.println("Hola");
+			}
+		}
 	}
 }
  
