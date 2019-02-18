6d47ecaef111ae65dbe72da800b91eb6605c4dda
APachon10 <Pachondestroy95@gmail.com>
2019-02-18 17:31:50 +0100

    Random

--- Practica3/vectors_clasesAbstractes/Nomina.java
+++ Practica3/vectors_clasesAbstractes/Nomina.java
@@ -1,5 +1,6 @@
 package vectors_clasesAbstractes;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Nomina{
@@ -11,16 +12,72 @@ public class Nomina{
 	}
 	//Metodos
 	public Tipus_Empleat insereixEmpleat(Tipus_Empleat e ) {
+		e= new Caixer(null, null, null, 0);
 		boolean salir=false;
+		int op=0;
+		String nom,lloc,ciutatO;
+		int horas=0,vendas=0;
+		int i=0;
 		do {
-			for (int i = 0; i < empleados.length; i++) {
-				if (i>empleados.length) {
+			if (i>empleados.length) {
+				salir=true;
+			}else {
+				if (empleados[i] == null) {
+					System.out.println("Que tipo de Empleado quieres crear? "
+							+ "\n1 - Caixer"
+							+ "\n2 - Mostrador"
+							+ "\n3 - Neteja ");
+					op=recogerEnteros();
+					switch (op) {
+					case 1:
+						System.out.print("Nombre Empleado:");
+						nom=recogerCadenas();
+						System.out.print("Ciudad Origen :");
+						ciutatO=recogerCadenas();
+						System.out.print("Lugar de Trabajo:");
+						lloc=recogerCadenas();
+						System.out.print("Horas Trabajadas:");
+						horas=recogerEnteros();
+						//Ponemos los Datos dentro del Objeto 
+						e = new Caixer(nom, ciutatO, lloc, horas);
+						empleados[i]=e;
+						System.out.println("===================");
+						System.out.println("Cajero Registrado");
+						System.out.println("===================");
+						break;
+					case 2:
+						System.out.print("Nombre Empleado:");
+						nom=recogerCadenas();
+						System.out.print("Ciudad Origen :");
+						ciutatO=recogerCadenas();
+						System.out.print("Lugar de Trabajo:");
+						lloc=recogerCadenas();
+						System.out.print("Numero de Ventas:");
+						vendas = recogerEnteros();
+						//Ponemos los Datos dentro del Objeto 
+						e = new Mostrador(nom, ciutatO, lloc, vendas);
+						empleados[i]=e;
+						System.out.println("===================");
+						System.out.println("Mostrador Registrado");
+						System.out.println("===================");
+						break;
+					case 3:
+						System.out.print("Nombre Empleado:");
+						nom=recogerCadenas();
+						System.out.print("Ciudad Origen :");
+						ciutatO=recogerCadenas();
+						System.out.print("Lugar de Trabajo:");
+						lloc=recogerCadenas();
+						e = new Neteja(nom, ciutatO, lloc);
+						empleados[i]=e;
+						System.out.println("===================");
+						System.out.println("Personal de Limpieza  Registrado");
+						System.out.println("===================");
+						break;
+					}
 					salir=true;
 				}else {
-					if (empleados[i] == null) {
-						empleados[i]=e;
-						salir=true;
-					}
+					i++;
 				}
 			}
 		}while (!salir);
@@ -56,10 +113,18 @@ public class Nomina{
 		}
 		System.out.println("El Numero de Cajeras es: "+cont);
 	}
+	public void mostrarDatos(Tipus_Empleat [] emple ) {
+		System.out.println(Arrays.toString(emple));
+	}
 	//Metodos para recogerDatps
 	public static int recogerEnteros() {
 		Scanner scan = new Scanner(System.in);
 		int numero = scan.nextInt();
 		return numero;
+	}
+	public static String recogerCadenas() {
+		Scanner scan = new Scanner(System.in);
+		String palabra = scan.nextLine();
+		return palabra;
 	}
 }
--- Practica3/vectors_clasesAbstractes/Principal.java
+++ Practica3/vectors_clasesAbstractes/Principal.java
@@ -6,6 +6,27 @@ public class Principal {
 	public static void main(String[] args) {
 		Tipus_Empleat empleados [] = new Tipus_Empleat[5];
 		Nomina m = new Nomina(empleados);
-		System.out.println(Arrays.toString(empleados));
+		int op=0;
+	
+		do {
+			System.out.println("Que Operacion quieres realizar?"
+					+ "\n1 - Añadir Empleado"
+					+ "\n2 - Eliminar Empleado de Limpieza de Nomina "
+					+ "\n3 - Ver Cuantas personas son de Cornella "
+					+ "\n4 - Ver cost total de la Nomina "
+					+ "\n5 - Ver cantidad de Cajeros en Nomina"
+					+ "\n6 - Sueldo Promedio"
+					+ "\n7 - Mostrar Array");
+			op=m.recogerEnteros();
+			switch (op) {
+			case 1:
+				Tipus_Empleat e = null;
+				m.insereixEmpleat(e);
+				break;
+			case 7:
+				m.mostrarDatos(empleados);
+				break;
+			}
+		} while (op!=0);
 	}
 }
