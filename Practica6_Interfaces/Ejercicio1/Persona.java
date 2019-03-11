c27a95bf1c637728e84083404b49800a0267d8d0
APachon10 <Pachondestroy95@gmail.com>
2019-03-11 17:32:52 +0100

    Nada

--- Practica6_Interfaces/Ejercicio1/Persona.java
+++ Practica6_Interfaces/Ejercicio1/Persona.java
@@ -4,32 +4,85 @@ import java.util.ArrayList;
 
 public class Persona implements Comparable<Persona>{
 	ArrayList<Persona > personas= new ArrayList<Persona>();
-	private String nom,edad;
-	private int alçada;
+	private String nom;
+	private int edad;
+	private double alçada;
 	//Constructor
-	public Persona(String nom, String edad, int alçada) {
+	
+	public Persona(String nom, int edad, double alçada) {
 		this.nom = nom;
 		this.edad = edad;
 		this.alçada = alçada;
+	}
+	public Persona(ArrayList<Persona> personas) {
+		this.personas = personas;
 	}
 	//Metodo para ordenar elementos 
 	@Override
 	public int compareTo(Persona o) {
 		int res =0;
 		if (this.edad > o.edad) {
-			res = 1;
 			
+			res =1;
+		}else if (this.edad<o.edad) {
+			res=0;
 		}
-		return 0;
+		return res;
 	}
 	//Metodos para gestionar El ArrayList
-	public void rellenarArrayList(ArrayList<Persona> people,Persona e) {
-		people.add(e);
+	public ArrayList<Persona> rellenarArrayList() {
+		Persona e = new Persona("Dani",22,1.87);
+		personas.add(e);
+		Persona e2=new Persona("Pol",52,1.73);
+		personas.add(e2);
+		
+		Persona e3=new Persona("Manel",27,1.58);
+		personas.add(e3);
+		
+		Persona e4=new Persona("David",25,1.64);
+		personas.add(e4);
+		
+		Persona e5=new Persona("Pere",80,1.84);
+		personas.add(e5);
+		return personas;
 	} 
+	public void insertarElemento(Persona e) {
+		personas.add(e);
+	}
 	public void mostrarDatos(ArrayList<Persona> people) {
 		for (int i = 0; i < people.size(); i++) {
 			System.out.println(people.get(i));
 		}
+	}
+	@Override
+	public String toString() {
+		return "nom=" + nom + ", edad=" + edad + ", alçada=" + alçada;
+	}
+	public ArrayList<Persona> getPersonas() {
+		return personas;
+	}
+	//Getters
+	public String getNom() {
+		return nom;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public double getAlçada() {
+		return alçada;
+	}
+	//Setters
+	public void setPersonas(ArrayList<Persona> personas) {
+		this.personas = personas;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public void setAlçada(double alçada) {
+		this.alçada = alçada;
 	}
 	
 }
--- Practica6_Interfaces/Ejercicio1/main.java
+++ Practica6_Interfaces/Ejercicio1/main.java
@@ -1,19 +1,38 @@
 package Ejercicio1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class main {
 	public static void main(String[] args) {
 		ArrayList<Persona> persona = new ArrayList<Persona>();
-		Persona p = new Persona(null,null,0);
-		
-		
+		ArrayList<Persona> persona_ordenadas = new ArrayList<Persona>();
+		Persona p = new Persona(null,0,0);
+		persona = p.rellenarArrayList();
+		System.out.println("========================");
 		System.out.println("Lista de Personas sin ordenar ");
 		System.out.println("========================");
 		p.mostrarDatos(persona);
 		System.out.println("========================");
+
+		Persona p1 = new Persona(persona.get(0).getNom(),persona.get(0).getEdad(),persona.get(0).getAlçada());
+
 		for (int i = 0; i < persona.size(); i++) {
-			
-		}
+			if (p1.compareTo(persona.get(i)) > 0 ) { 
+				System.out.println("La persona p1: es mayor. " +p1.toString()); 
+			}else if (p1.compareTo(persona.get(i)) < 0 ) {
+				System.out.println("La persona p1: es menor. "+p1.toString()); 
+			}else { 
+				System.out.println ("La persona p1 es igual a la persona p2: " +p1.toString()); 
+			}
+		} 
+
+		System.out.println("Lista de Personas ordenas por Altura");
+		System.out.println("=========================");
+		p.mostrarDatos(persona_ordenadas);
+		System.out.println("========================");
+		System.out.println();
+		System.out.println("Programa Finalizado ");
 	}
 }
